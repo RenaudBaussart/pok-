b2bde1cacc0c8b5a21bc4b6f446079081a8cd9eb
Hugo-walando <hugowalando@gmail.com>
2024-06-21 12:07:14 +0200

    feat: Display pokemons from ap & import HttpClient provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PokemonListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'poke-app';
---
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-pokemon-list',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './pokemon-list.component.html',
+  styleUrl: './pokemon-list.component.scss',
+})
+export class PokemonListComponent implements OnInit {
+  pokemons: any = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getPokemons().subscribe((data) => {
+      this.pokemons = data.results;
+    });
+  }
+}
