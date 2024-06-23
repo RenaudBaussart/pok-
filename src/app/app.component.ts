1449dc97dc76dab596f6494c2ca40978f49670db
Hugo-walando <hugowalando@gmail.com>
2024-06-23 12:30:08 +0200

    feat: import pokemonDetailComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PokemonListComponent],
+  imports: [RouterOutlet, PokemonListComponent, PokemonDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
