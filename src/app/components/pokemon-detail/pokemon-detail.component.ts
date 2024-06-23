3182b98b34834b3762851e353e1385e108c09f03
Hugo-walando <hugowalando@gmail.com>
2024-06-23 11:51:00 +0200

    feat: add pokemon-detail component

---
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-detail.component.html',
+  styleUrl: './pokemon-detail.component.scss'
+})
+export class PokemonDetailComponent {
+
+}
