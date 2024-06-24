651c050ac8e0f70aa607bc8bb1f6ffabd0f0e716
Hugo-walando <hugowalando@gmail.com>
2024-06-24 17:21:56 +0200

    feat: add getBackGroundColor method

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { PokemonModel } from '../../pokemon-model';
-import { NgIf } from '@angular/common';
+import { NgIf, NgStyle } from '@angular/common';
+import { getTypeColor } from '../../types-colors';
 
 @Component({
   selector: 'app-pokemon-detail',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgStyle],
   templateUrl: './pokemon-detail.component.html',
   styleUrl: './pokemon-detail.component.scss',
 })
@@ -20,8 +21,15 @@ export class PokemonDetailComponent {
       this.selectedPokemon = pokemon;
     });
   }
-  statBarPercentage(currentStat: number){
-    return (currentStat * 100) / 255
+  statBarPercentage(currentStat: number) {
+    return (currentStat * 100) / 255;
+  }
 
+  getBackgroundColor(): string {
+    if (this.selectedPokemon && this.selectedPokemon.types.length > 0) {
+      const primaryType = this.selectedPokemon.types[0].name.toLowerCase();
+      return getTypeColor(primaryType);
+    }
+    return 'white'; // couleur par défaut
   }
 }
